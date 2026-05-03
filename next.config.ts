592b3dd5e17567ddc0cd46ff78f3f5e58f072afe
msharafeddine8-passion <msharafeddine8@gmail.com>
2026-05-03 16:50:53 +0300

    fix next config

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["*.supabase.co", "lh3.googleusercontent.com"],
-  },
-};
-
-export default nextConfig;
